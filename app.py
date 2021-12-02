2b19baea3c4c234ab1408a83b9cf8644ae417fad
SteveKennyUK <steven.kenny@hotmail.co.uk>
2021-12-02 23:00:04 +0000

    set nav link displays for session and admin users

--- app.py
+++ app.py
@@ -77,11 +77,17 @@ def login():
             # ensure hashed password matches user input
             if check_password_hash(existing_user["password"], request.form.get(
                     "password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash(f"""Arooo! Welcome back {request.form.get(
-                        'username')}""")
-                    return redirect(url_for(
-                        "profile", username=session["user"]))
+                session["user"] = request.form.get("username").lower()
+                # create session admin user/s (
+                # Credit: https://github.com/irasan/hackpride2021)
+                check_admin = mongo.db.users.find_one(
+                        {"username": request.form.get("username").lower()})
+                if check_admin["is_admin"]:
+                        session["admin"] = True
+                flash(f"""Arooo! Welcome back {request.form.get(
+                    'username')}""")
+                return redirect(url_for(
+                    "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -113,7 +119,7 @@ def logout():
     logout user by removing session cookies
     """
     flash("You have been logged out")
-    session.pop("user")
+    session.clear()
     return redirect(url_for("login"))
 
 
