31bbcb85a1836aebec73b9e1dc8c6040aa7c0611
SteveKennyUK <steven.kenny@hotmail.co.uk>
2021-12-03 00:59:39 +0000

    create add_dog template and view

--- app.py
+++ app.py
@@ -140,6 +140,13 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_dog")
+@login_required
+def add_dog():
+    breeds = mongo.db.breed_groups.find().sort("breed_name", 1)
+    return render_template("add_dog.html", breeds=breeds)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,5 @@ $(document).ready(function () {
     $('.dropdown-trigger').dropdown({ coverTrigger: false });
     $('.sidenav').sidenav({edge: "right"});
     $('.tooltipped').tooltip();
+    $('select').formSelect();
 });
